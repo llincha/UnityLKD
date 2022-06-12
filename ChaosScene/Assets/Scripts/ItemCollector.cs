324a6f3074a9dec38a09223eb10eb97e8015c1d5
llincha <lin4a.bbc@gmail.com>
2022-06-13 02:03:50 +0300

    Uploaded game

---
+++ ChaosScene/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    int coinCount = 0;
+
+    [SerializeField] Text  coinsText;
+    [SerializeField] AudioSource coinSound;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            coinCount++;
+            coinsText.text = "Coins: " + coinCount;
+            coinSound.Play();
+        }
+    }
+}
---
+++ ChaosScene/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    [SerializeField] AudioSource deathSound;
+
+    bool dead = false;
+
+    void Update()
+    {
+        if (transform.position.y < -1f && !dead)
+        {
+            Die();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy Body"))
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<PlayerControls>().enabled = false;
+            Die();
+        }
+    }
+
+    void Die() 
+    {
+        dead = true;
+        deathSound.Play();
+        Invoke(nameof(ReloadLevel), 1.3f);
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
